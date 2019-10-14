cd41f1295a7b2b9888fcf4eb37b03c09a2082983
330775892@qq.com <luokeli1989>
2019-10-15 00:10:39 +0800

    一个资源提供给N个团队调用，此时需要对其中三个origin进行流控，且流控的规则一致，则只需要在【针对来源录入多个origin，之间以英文逗号分割】 流控flow解析时扩展针对来源，多个以英文逗号分割。 解析时，将平铺每个limitApp。 即 资源A：limitApp1-rule       limitApp2-rule

--- sentinel-demo/sentinel-demo-basic/src/main/java/com/alibaba/csp/sentinel/demo/flow/FlowQpsDemo.java
+++ sentinel-demo/sentinel-demo-basic/src/main/java/com/alibaba/csp/sentinel/demo/flow/FlowQpsDemo.java
@@ -21,6 +21,8 @@ import java.util.Random;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import com.alibaba.csp.sentinel.context.Context;
+import com.alibaba.csp.sentinel.context.ContextUtil;
 import com.alibaba.csp.sentinel.util.TimeUtil;
 import com.alibaba.csp.sentinel.Entry;
 import com.alibaba.csp.sentinel.SphU;
@@ -65,7 +67,7 @@ public class FlowQpsDemo {
         // set limit qps to 20
         rule1.setCount(20);
         rule1.setGrade(RuleConstant.FLOW_GRADE_QPS);
-        rule1.setLimitApp("default");
+        rule1.setLimitApp("a,b");
         rules.add(rule1);
         FlowRuleManager.loadRules(rules);
     }
@@ -136,6 +138,8 @@ public class FlowQpsDemo {
                 Entry entry = null;
 
                 try {
+                    Context context = ContextUtil.enter("testDemo", "a,b");
+                    //System.out.println(context);
                     entry = SphU.entry(KEY);
                     // token acquired, means pass
                     pass.addAndGet(1);
@@ -148,6 +152,7 @@ public class FlowQpsDemo {
                     if (entry != null) {
                         entry.exit();
                     }
+                    ContextUtil.exit();
                 }
 
                 Random random2 = new Random();
